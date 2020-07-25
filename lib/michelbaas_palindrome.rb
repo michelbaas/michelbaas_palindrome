1439ebb9eebd73eaf02d23ae2c08e9fecd4b77b4
Michel Baas <michelbaas@gmail.com>
2020-07-25 13:14:06 +0200

    Finish work and refactor palindrome method

--- lib/michelbaas_palindrome.rb
+++ lib/michelbaas_palindrome.rb
@@ -1,6 +1,21 @@
 require "michelbaas_palindrome/version"
 
-module MichelbaasPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # # Returns the letters in the string.
+  # def letters
+  #   self.chars.select { |c| c.match(/[a-z]/i) }.join
+  # end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      downcase.scan(/[a-z]/i).join
+    end
 end
